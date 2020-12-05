709d3c1ef87fcf3f62ad48e70b624f5ef22d9dcf
iambrj <joshibharathiramana@gmail.com>
2020-12-05 20:54:19 +0530

    eliza: add comments

--- eliza/eliza.rkt
+++ eliza/eliza.rkt
@@ -1,8 +1,10 @@
 #lang racket
 
+; select random element from list l
 (define (random-elt l)
   (list-ref l (random (length l))))
 
+; perform substitutions in tree according to the association list
 (define (sublis al tree)
   (cond
     [(cons? tree)
@@ -12,6 +14,7 @@
        (if p (cdr p) tree))]
     [else tree]))
 
+; symbol and list equality
 (define (simple-equal? x y)
   (if (or (symbol? x) (symbol? y))
     (eq? x y)
@@ -22,35 +25,46 @@
 
 (define no-bindings '((t . t)))
 
+; get pair with key var in the association list bindings
 (define (get-binding var bindings)
   (assoc var bindings))
 
+; get value for the pair binding
 (define (binding-val binding)
   (cdr binding))
 
+; get value for the key var in the association list bindings
 (define (lookup var bindings)
   (binding-val (get-binding var bindings)))
 
+; add new key value pair to the association list
 (define (extend-bindings var val bindings)
   `((,var . ,val) . ,(if (equal? bindings no-bindings) empty bindings)))
 
 (define (pat-match pattern input [bindings no-bindings])
   (cond
+    ; failure only occurs when bindings are empty
     [(eq? bindings fail) fail]
+    ; no more elements to match, return current bindings
     [(empty? pattern) bindings]
+    ; try to match using simple equality
     [(simple-equal? pattern input) bindings]
     [(variable? pattern) (match-variable pattern input bindings)]
     [(segment-pattern? pattern) (segment-match pattern input bindings)]
+    ; recur if nothing works out
     [(and (cons? pattern) (cons? input)) (pat-match (cdr pattern)
                                                     (cdr input)
                                                     (pat-match (car pattern)
                                                                (car input)
                                                                bindings))]
+    ; give up and just fail
     [else fail]))
 
+; variables are of the form '?x, '?y etc
 (define (variable? x)
   (and (symbol? x) (eq? (string-ref (symbol->string x) 0) #\?)))
 
+; segment-variables are of the form '(?* ?x), '(?* ?y) etc
 (define (segment-variable? x)
   (and (list? x)
        (equal? (length x) 2)
@@ -58,9 +72,15 @@
        (symbol? (cadr x))
        (variable? (cadr x))))
 
+; segment-patterns are of the form '((?* ?x) hello there),
+; '((?* ?x) i feel happy)
 (define (segment-pattern? p)
   (and (list? p) (segment-variable? (car p))))
 
+; Incrementally match pattern against input to see if any of the bindings work
+; out. For example, segment matching '((?* ?x) a b (?* ?x)) against
+; '(c d a b a b c d a b) results in first binding '(x . (c d)), which fails in
+; the recursive call and next binding '(x . (c d a b)) which works out.
 (define (segment-match pattern input bindings [start 0])
   (let ([var (second (first pattern))]
         [pat (rest pattern)])
@@ -89,6 +109,8 @@
          (if (zero? s) c (position e t (- s 1) (+ c 1)))
          (position e t s (+ c 1))))]))
 
+; if matching binding already exsist for var, return bindings as it is. If it
+; doesn't exist and it and return new binding
 (define (match-variable var input bindings)
   (let ([binding (get-binding var bindings)])
     (cond [(not binding) (extend-bindings var input bindings)]

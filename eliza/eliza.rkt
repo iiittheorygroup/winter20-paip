655bec4ef316fba8f0205ddc79f186b4f6942942
iambrj <joshibharathiramana@gmail.com>
2020-12-05 14:05:02 +0530

    eliza: Implement simple segment pattern matching

--- eliza/eliza.rkt
+++ eliza/eliza.rkt
@@ -28,6 +28,7 @@
     [(empty? pattern) bindings]
     [(simple-equal? pattern input) bindings]
     [(variable? pattern) (match-variable pattern input bindings)]
+    [(segment-pattern? pattern) (segment-match pattern input bindings)]
     [(and (cons? pattern) (cons? input)) (pat-match (cdr pattern)
                                                     (cdr input)
                                                     (pat-match (car pattern)
@@ -37,6 +38,44 @@
 
 (define (variable? x)
   (and (symbol? x) (eq? (string-ref (symbol->string x) 0) #\?)))
+
+(define (segment-variable? x)
+  (and (list? x)
+       (equal? (length x) 2)
+       (equal? (symbol->string (car x)) "?*")
+       (symbol? (cadr x))
+       (variable? (cadr x))))
+
+(define (segment-pattern? p)
+  (segment-variable? (car p)))
+
+(define (segment-match pattern input bindings [start 0])
+  (let ([var (second (first pattern))]
+        [pat (rest pattern)])
+    (if (empty? pat)
+      (match-variable var input bindings)
+      (let ([pos (position (first pat) input start 1)])
+        (if (zero? pos)
+          fail
+          (let ([b2 (pat-match pat
+                               (drop input (- pos 1))
+                               (match-variable var
+                                               (take input (- pos 1))
+                                               bindings))])
+            (if (equal? b2 fail)
+              (segment-match pattern input bindings (+ pos 1))
+              b2)))))))
+
+; return position of element e in list l after skipping s occurrences
+; c = current position in list
+(define (position e l s c)
+  (cond
+    [(empty? l) 0]
+    [(let ([h (car l)]
+           [t (cdr l)])
+       (if (equal? e h)
+         (if (zero? s) c (position e t (- s 1) (+ c 1)))
+         (position e t s (+ c 1))))]))
 
 (define (match-variable var input bindings)
   (let ([binding (get-binding var bindings)])
